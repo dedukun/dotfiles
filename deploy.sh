c29ad4bad29d907c16f6138840f95235224f7a56
Diogo Duarte <dierduarte@gmail.com>
2019-07-08 23:32:54 +0100

    new prints + added st requirements

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ _error() {
 }
 
 _install_python3() {
-    echo "Installing python3..."
+    printf "\nInstalling python3...\n"
 
     # install support packages
     apt install python3 python3-pip python3-dev
@@ -15,7 +15,7 @@ _install_python3() {
 }
 
 _install_mpsyt() {
-    echo "Installing mpsyt..."
+    printf "\nInstalling mpsyt...\n"
 
     # install virtualenvwrapper
     python3 -m pip install virtualenvwrapper
@@ -28,13 +28,16 @@ _install_mpsyt() {
 }
 
 _install_nvim() {
-    echo "Installing nvim..."
+    printf "\nInstalling nvim...\n"
     mkdir -p "$HOME/.local/bin"
     wget https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -O "$HOME/.local/bin/nvim" --quiet --show-progress
 }
 
 _install_st() {
-    echo "Installing st..."
+    printf "\nInstalling st...\n"
+    # installing x11 headers
+    apt install libx11-dev libxft-dev
+
     git clone https://github.com/lukesmithxyz/st /tmp/st-luke
     cd /tmp/st-luke || { _error "Can't cd into '/tmp/st-luke'"; return $?; }
 
@@ -45,12 +48,13 @@ _install_st() {
 }
 
 _install_basics() {
-    echo "Installing basics..."
+    printf "\nInstalling basics...\n"
     apt update
     apt install build-essential git
 }
 
 _install_dotfiles(){
+    printf "\nInstalling dotfiles...\n"
     git clone https://github.com/lukesmithxyz/st /tmp/dotfiles
     cd /tmp/dotfiles || { _error "Can't cd into '/tmp/dotfiles'"; return $?; }
 
