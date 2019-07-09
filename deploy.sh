496e3dd96e366c050c991333bea3b40aea41d391
Diogo Duarte <dierduarte@gmail.com>
2019-07-09 01:30:30 +0100

    Update deploy (v0.1)
        - Install pip packages in user
        - Fixed nvim install
        - Added shellsheck
        - Fix dot file permissions
        - mpsyt auto install is WIP

--- deploy.sh
+++ deploy.sh
@@ -11,14 +11,14 @@ _install_python3() {
     apt install python3 python3-pip python3-dev -y
 
     # Upgrade pip
-    python3 -m pip install --upgrade pip
+    runuser -l "$usr_name" -c "python3 -m pip install --upgrade pip"
 }
 
 _install_mpsyt() {
     printf "\nInstalling mpsyt...\n"
 
     # install virtualenvwrapper
-    python3 -m pip install virtualenvwrapper
+    runuser -l "$usr_name" -c "python3 -m pip install virtualenvwrapper"
 
     workonenv
     mkvirutalenv youtube
@@ -30,19 +30,19 @@ _install_mpsyt() {
 _install_nvim() {
     printf "\nInstalling nvim...\n"
 
-    python3 -m install neovim
+    runuser -l "$usr_name" -c "python3 -m install neovim"
 
     mkdir -p "$usr_home/.local/bin"
     wget https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -O "$usr_home/.local/bin/nvim" --quiet --show-progress
 
     # Install plugs
-    nvim -c PlugInstall -c quit -c quit
+    runuser -l "$usr_name" -c "$usr_home/.local/bin/nvim -c PlugInstall -c quit -c quit"
 
     # Install youcompleteme
     apt install cmake -y
 
     cd "$usr_home/.vim/plugged/YouCompleteMe" || { _error "Can't cd into '$HOME/.vim/plugged/YouCompleteMe'"; return $?; }
-    python3 install.py --clang --ts-completer --java-completer
+    python3 install.py --clang-completer --ts-completer --java-completer
 
     cd "$usr_home" || { _error "Can't cd into '$HOME'"; return $?; }
 }
@@ -70,23 +70,27 @@ _install_basics() {
     printf "\nInstalling basics...\n"
     apt update
     apt install build-essential git -y
-    apt install node npm -y
+    apt install npm -y
     apt install default-jre default-jdk -y
+    apt install shellsheck -y
 }
 
 _install_dotfiles(){
     printf "\nInstalling dotfiles...\n"
+
+    chown -R "$usr_name:$usr_name" /tmp/dotfiles
+
     git clone https://github.com/dedukun/dotfiles /tmp/dotfiles
     cd /tmp/dotfiles || { _error "Can't cd into '/tmp/dotfiles'"; return $?; }
 
-    cp -r ".scripts" "$usr_home"
-    cp    ".bashrc"  "$usr_home"
-    cp    ".profile" "$usr_home"
-    cp    ".inputrc" "$usr_home"
-    cp    ".xinitrc" "$usr_home"
-    cp    ".pythonrc.py" "$usr_home"
-    cp -r ".config/" "$usr_home"
-    cp -r ".local/" "$usr_home"
+    cp -p -r ".scripts" "$usr_home"
+    cp -p    ".bashrc"  "$usr_home"
+    cp -p    ".profile" "$usr_home"
+    cp -p    ".inputrc" "$usr_home"
+    cp -p    ".xinitrc" "$usr_home"
+    cp -p    ".pythonrc.py" "$usr_home"
+    cp -p -r ".config/" "$usr_home"
+    cp -p -r ".local/" "$usr_home"
 
     . "$usr_home/.profile"
 
@@ -101,13 +105,14 @@ _install_dotfiles(){
 # run as root
 if [ ! "$(whoami)" = root ]; then
     # save original home directory
-    echo "$HOME" >> /tmp/dotfiles-deploy.sh
+    echo "$USER" >> /tmp/dotfiles-deploy.sh
     sudo "$0" "$@"
     exit $?
 fi
 
 if [ -e /tmp/dotfiles-deploy.sh ]; then
-    usr_home="$(cat /tmp/dotfiles-deploy.sh)"
+    usr_name="$(cat /tmp/dotfiles-deploy.sh)"
+    usr_home="/home/$usr_name"
     rm /tmp/dotfiles-deploy.sh
 else
     usr_home="$HOME"
@@ -118,7 +123,7 @@ printf "Deploying configs into '%s'...\n" "$usr_home"
 _install_basics
 _install_dotfiles
 _install_python3
-_install_mpsyt
+# _install_mpsyt WIP
 _install_nvim
 _install_st
 _install_i3
