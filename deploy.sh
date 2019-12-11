dbe32970a6ce9da615808a4c99fdded4edf46c94
Diogo Duarte <dierduarte@gmail.com>
2019-12-11 16:11:58 +0000

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -37,6 +37,20 @@ _error() {
     printf "\033[31mERROR:\033[0m %s\n" "$1"
 }
 
+_check_non_free_repositories() {
+    if [ $(grep -q "non-free" /etc/apt/sources.list) ]; then
+        printf "Contrib and non-free repositories are not active."
+        read -r -p "\nThis script needs them to install some of the packages,
+want to activate them? [Y/n] " activate_non_free
+
+        if [ $(echo "$activate_non_free" | grep -q -e "^[yY]?$") -gt 0 ]; then
+            printf "OK"
+        else
+            printf "ERROR"
+        fi
+    fi
+}
+
 #######################
 ## INSTALL FUNCTIONS ##
 #######################
@@ -49,13 +63,19 @@ _install_python3() {
 
     # upgrade pip
     runuser -l "$usr_name" -c "python3 -m pip install --upgrade pip"
-
+f
     # install virtualenvwrapper
     runuser -l "$usr_name" -c "python3 -m pip install --user virtualenvwrapper"
 }
 
 _install_nvim_latest() {
     printf "\nInstalling nvim latest...\n"
+
+    # add fuse
+    apt install fuse
+    modprobe fuse
+    groupadd fuse
+    usermod -a -G fuse "$user_name"
 
     runuser -l "$usr_name" -c "mkdir -p $usr_home/.local/bin"
     wget https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -O "$usr_home/.local/bin/nvim" --quiet --show-progress
@@ -69,7 +89,7 @@ _install_nvim_latest() {
     runuser -l "$usr_name" -c "python3 -m pip install --user neovim-remote"
 
     # Install plugs
-    runuser -l "$usr_name" -c "$usr_home/.local/bin/nvim -c PlugInstall -c quit -c quit"
+    runuser -l "$usr_name" -c "$usr_home/.local/bin/nvim -c PlugInstall -c UpdateRemotePlugins -c quit -c quit"
 
     # shell scripts analysis tool
     apt install shellcheck -y
@@ -80,11 +100,11 @@ _install_nvim_latest() {
     # Latex for vimtex
     apt install latexmk -y
 
+    runuser -l "$usr_name" -c "python3 -m pip install --user pynvim neovim"
+    runuser -l "$usr_name" -c "npm install -g neovim"
+
     # install nauniq (used in custom fzf_history custom command (in .bashrc) to remove repeated entries)
     PERL_MM_USE_DEFAULT=1 perl -MCPAN -e 'install App::nauniq'
-
-    # install youcompleteme completer's
-    runuser -l "$usr_name" -c "python3 $usr_home/.vim/plugged/YouCompleteMe/install.py --clang-completer --ts-completer --java-completer"
 }
 
 _install_st() {
@@ -93,18 +113,23 @@ _install_st() {
     # installing x11 headers
     apt install libx11-dev libxft-dev -y
 
-    git clone https://github.com/lukesmithxyz/st /tmp/st-luke
-    cd /tmp/st-luke || { _error "Can't cd into '/tmp/st-luke'"; return $?; }
+    git clone https://github.com/dedukun/st /tmp/st-dedu
+    cd /tmp/st-dedu || {
+        _error "Can't cd into '/tmp/st-dedu'"
+        return $?
+    }
 
     make install
 
-    cd "$usr_home" || { _error "Can't cd into '$usr_home'"; return $?; }
-    rm -rf /tmp/st-luke
+    cd "$usr_home" || {
+        _error "Can't cd into '$usr_home'"
+        return $?
+    }
+    rm -rf /tmp/st-dedu
 }
 
 _install_i3() {
     printf "\nInstalling i3...\n"
-    apt install suckless-tools -y           # dmenu | slock
     apt install i3-wm i3status -y
 
     runuser -l "$usr_name" -c "mkdir -p $usr_home/Applications"
@@ -112,10 +137,16 @@ _install_i3() {
     # xcwd (used in i3 to check the directory of the current terminal)  [Control+mod+Enter]
     apt install libX11-dev
     runuser -l "$usr_name" -c "git clone https://github.com/schischi/xcwd.git $usr_home/Applications/xcwd"
-    cd $usr_home/Applications/xcwd || { _error "Can't cd into '$usr_home/Applications/xcwd'"; return $?; }
+    cd $usr_home/Applications/xcwd || {
+        _error "Can't cd into '$usr_home/Applications/xcwd'"
+        return $?
+    }
     runuser -l "$usr_name" -c "make"
     make install
-    cd "$usr_home" || { _error "Can't cd into '$usr_home'"; return $?; }
+    cd "$usr_home" || {
+        _error "Can't cd into '$usr_home'"
+        return $?
+    }
 
     # dmenu network manager [mod+F2]
     runuser -l "$usr_name" -c "git clone https://github.com/firecat53/networkmanager-dmenu.git $usr_home/Applications/networkmanager-dmenu"
@@ -123,15 +154,16 @@ _install_i3() {
 
 _install_basics() {
     printf "\nInstalling basics...\n"
-    apt install build-essential -y              #
-    apt install cmake -y                        #
-    apt install git -y                          #
+    apt install build-essential -y #
+    apt install cmake -y           #
+    apt install git -y             #
 }
 
 _install_extra_packages() {
     printf "\nInstalling extras packages...\n"
     apt install xserver-xorg-input-synaptics -y # synclient (to config touchpad)
     apt install libnotify4 libnotify-bin -y     # notify-send
+    apt install suckless-tools -y               # dmenu | slock
     apt install thunderbird -y                  # mail client
     apt install xbacklight -y                   #
     apt install unclutter -y                    # puts the mouse invisible
@@ -153,9 +185,9 @@ _install_extra_packages() {
 
 _install_extra_languages() {
     printf "\nInstalling extras languages...\n"
-    apt install default-jre default-jdk -y  # Java
-    apt install npm -y                      # Node
-    apt install perl -y                     # Perl
+    apt install default-jre default-jdk -y # Java
+    apt install npm -y                     # Node
+    apt install perl -y                    # Perl
 }
 
 _install_fonts() {
@@ -163,33 +195,39 @@ _install_fonts() {
     apt install ttf-unifont fonts-cmu fonts-oldstandard mathematica-fonts msttcorefonts ttf-dejavu ttf-bitstream-vera fonts-powerline -y
 }
 
-_install_dotfiles(){
+_install_dotfiles() {
     printf "\nInstalling dotfiles...\n"
 
     git clone https://github.com/dedukun/dotfiles /tmp/dotfiles
 
     chown -R "$usr_name:$usr_name" /tmp/dotfiles
 
-    cd /tmp/dotfiles || { _error "Can't cd into '/tmp/dotfiles'"; return $?; }
+    cd /tmp/dotfiles || {
+        _error "Can't cd into '/tmp/dotfiles'"
+        return $?
+    }
 
     #find . -maxdepth 1 -name "\.*" -and -not -name ".git*" -type f | tail -n +2 | xargs cp    - "$usr_home"
     #find . -maxdepth 1 -name "\.*" -and -not -name ".git*" -type d | tail -n +2 | xargs cp -r - "$usr_home"
     cp -p -r ".scripts" "$usr_home"
-    cp -p    ".bashrc"  "$usr_home"
-    cp -p    ".profile" "$usr_home"
-    cp -p    ".inputrc" "$usr_home"
-    cp -p    ".xinitrc" "$usr_home"
-    cp -p    ".screenrc" "$usr_home"
-    cp -p    ".pythonrc.py" "$usr_home"
-    cp -p    ".xbindkeysrc" "$usr_home"
+    cp -p ".bashrc" "$usr_home"
+    cp -p ".profile" "$usr_home"
+    cp -p ".inputrc" "$usr_home"
+    cp -p ".xinitrc" "$usr_home"
+    cp -p ".screenrc" "$usr_home"
+    cp -p ".pythonrc.py" "$usr_home"
+    cp -p ".xbindkeysrc" "$usr_home"
     cp -p -r ".config/" "$usr_home"
     cp -p -r ".local/" "$usr_home"
-    ln -s    "$usr_home/.config/nvim" "$usr_home/.vim"
-    ln -s    "$usr_home/.config/nvim/init.vim" "$usr_home/.vimrc"
+    ln -s "$usr_home/.config/nvim" "$usr_home/.vim"
+    ln -s "$usr_home/.config/nvim/init.vim" "$usr_home/.vimrc"
 
     . "$usr_home/.profile"
 
-    cd "$usr_home" || { _error "Can't cd into '$usr_home'"; return $?; }
+    cd "$usr_home" || {
+        _error "Can't cd into '$usr_home'"
+        return $?
+    }
     rm -rf /tmp/dotfiles
 }
 
@@ -203,19 +241,21 @@ _install_scripts() {
     ln -s "$usr_scripts/other/countdown.sh" "$usr_scripts_bin/countdown"
     ln -s "$usr_scripts/second.sh" "$usr_scripts_bin/csd"
     ln -s "$usr_scripts/display.sh" "$usr_scripts_bin/csm"
-    ln -s "$usr_scripts/gbt/logs.sh"    "$usr_scripts_bin/glbt_log"
-    ln -s "$usr_scripts/gbt/move.sh"   "$usr_scripts_bin/glbt_mov"
+    ln -s "$usr_scripts/gbt/logs.sh" "$usr_scripts_bin/glbt_log"
+    ln -s "$usr_scripts/gbt/move.sh" "$usr_scripts_bin/glbt_mov"
     ln -s "$usr_scripts/gbt/outputs.sh" "$usr_scripts_bin/glbt_out"
     ln -s "$usr_scripts/gbt/project.sh" "$usr_scripts_bin/glbt_proj"
     ln -s "$usr_scripts/locate_menu.sh" "$usr_scripts_bin/lome"
     ln -s "$usr_scripts/remove_white_spaces.sh" "$usr_scripts_bin/rws"
+    ln -s "$usr_scripts/i3/background.sh" "$usr_scripts_bin/mtb"
+    ln -s "$usr_scripts/tempdir.sh" "$usr_scripts_bin/mtd"
 
     # make sure that everything in the user's home is owned by the user
     chown -R "$usr_name:$usr_name" "$usr_home"
 }
 
 _uninstall_undesired_packages() {
-    apt purge gdm3 gnome lightdm -y
+    apt purge gnome gdm3 lightdm -y
     apt purge evolution -y
     apt purge evince -y
     apt autoremove -y
@@ -267,35 +307,34 @@ install_dot() {
 parse_args() {
     [ $# -lt 1 ] && echo "Need at least one argument!\nFor more information see the '--help' page." && exit 1
 
-    while [ $# -gt 0 ]
-    do
+    while [ $# -gt 0 ]; do
         dp_key="$1"
 
         case "$dp_key" in
-            -a|--all)
-                shift # past argument
-                usr_all="True"
-                ;;
-            -d|--dotfiles)
-                shift # past argument
-                usr_dot="True"
-                ;;
-            -u|--user)
-                _set_user "$2"
-                shift # past argument
-                shift # past value
-                ;;
-            -h|--help)
-                shift # past argument
-                print_help
-                exit 0
-                ;;
-            *)
-                echo "Invalid argument '$1'."
-                echo "For more help use argument -h or --help".
-                shift # past argument
-                exit 1
-                ;;
+        -a | --all)
+            shift # past argument
+            usr_all="True"
+            ;;
+        -d | --dotfiles)
+            shift # past argument
+            usr_dot="True"
+            ;;
+        -u | --user)
+            _set_user "$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -h | --help)
+            shift # past argument
+            print_help
+            exit 0
+            ;;
+        *)
+            echo "Invalid argument '$1'."
+            echo "For more help use argument -h or --help".
+            shift # past argument
+            exit 1
+            ;;
         esac
     done
 }
@@ -309,10 +348,10 @@ parse_args "$@"
 apt update
 _run_as_root "$@"
 
-if [ "$usr_all" = "True" ];then
+if [ "$usr_all" = "True" ]; then
     install_all
 else
-    if [ "$usr_dot" = "True" ];then
+    if [ "$usr_dot" = "True" ]; then
         install_dot
     fi
 fi
